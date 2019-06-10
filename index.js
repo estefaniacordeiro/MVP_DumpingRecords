d820b9ec970a3994cc308b1a8ed4ef6a7280af05
bos25 <estefania.cbrion@gmail.com>
2019-06-11 01:17:19 +0200

    Connection to mysql created and running

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 'use strict';
 
-// require("dotenv").config();
+require("dotenv").config();
 const bodyParser = require('body-parser');
 const express = require('express');
+const mysqlPool = require("./database/mysql-pool");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running and listening on port ${port}`);
-});
+async function init() {
+    try {
+        await mysqlPool.connect();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`Server running and listening on port ${port}`);
+    });
+}
+
+init();
 
 
 
